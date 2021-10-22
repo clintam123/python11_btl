be39df5a7fac85fb6ddcc31808bcbb8b5e273d23
truongnq2001 <90451139+truongnq2001@users.noreply.github.com>
2021-10-22 18:16:03 +0700

    Update views.py

--- blog_foods/views.py
+++ blog_foods/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from .models import Post
 from django.views import generic
@@ -19,4 +20,8 @@ class DetailView(generic.DetailView):
 def author(request, user_id):
     singleAuthor = User.objects.get(id=user_id)
     posts = Post.objects.filter(posted_by_id=user_id)
-    return render(request, 'review/author.html', {'singleAuthor': singleAuthor, 'posts': posts})
+    return render(request, 'author.html', {'singleAuthor': singleAuthor, 'posts': posts})
+
+def post(request, post_id):
+    singlePost = Post.objects.get(id=post_id)
+    return render(request, 'post.html', {'singlePost': singlePost})
