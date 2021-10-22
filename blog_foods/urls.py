6ab827e3cc769b53ef00e5c52e656d0f6a97695e
truongnq2001 <90451139+truongnq2001@users.noreply.github.com>
2021-10-22 18:13:39 +0700

    Update urls.py

--- blog_foods/urls.py
+++ blog_foods/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('upload/', views.upload, name='upload'),  # 'Upload'
     path('<int:pk>', views.DetailView.as_view(), name='detail'),
     path('author/<int:user_id>', views.author, name='author_detail'),
+    path('post/<int:post_id>', views.post, name='post_detail'),
 ]
