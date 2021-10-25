713baa6bccda1dab7c7c1084536d35c04afbbe94
Luc Nguyen Van <luccp2@gmail.com>
2021-10-25 21:09:16 +0700

    delete

--- blog_foods/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Profile
-from .models import Post
-from .models import LikePost
-# Register your models here.
-
-admin.site.register(Profile)
-admin.site.register(Post)
-admin.site.register(LikePost)
--- blog_foods/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BlogFoodsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog_foods'
